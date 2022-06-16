5dcd7bddc87439c0948add662eb36af518b68636
George J Padayatti <george.padayatti@igrant.io>
2022-06-16 22:21:25 +0530

    Check if repo has changes in before executing git commands
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
-if ! (git diff --exit-code origin/master..master > /dev/null) \
-  || ! (git diff --exit-code master > /dev/null) \
+if ! (git diff --exit-code origin/main..main > /dev/null) \
+  || ! (git diff --exit-code main > /dev/null) \
   || ! [[ -z "$(git status --porcelain)" ]] ; then
   echo "Your local repo has some changes that aren't pushed to origin/master ."
   exit 1
